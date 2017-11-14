f1b261a29a816c88ac35c4d703b3646e7a8e98f3
Amanda K <kosateam@yahoo.com>
2017-11-14 10:21:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,12 +1,12 @@
-var playlist = new Object({
+var playlist = {
   brunoMars: "24K Magic"
-})
+}
 
 function updatePlaylist(obj, key, value){
   return Object.assign({}, obj, {[key]: value})
 }
 
-function removeFromPlaylist(playlistObject, artistName){
-  delete playlistObject.artistName
-  return playlistObject
+function removeFromPlaylist(playlistObj, artistName){
+  delete playlistObj.artistName
+  return playlistObj
 }
